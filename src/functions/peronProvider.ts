1121513b65a795e093df15739e0bdc3317dd7033
aaronsum1102 <sumsx03@gmail.com>
2021-03-26 17:13:27 +0100

    Add pfunction to sent person with event

--- src/functions/peronProvider.ts
+++ src/functions/peronProvider.ts
@@ -1,13 +1,30 @@
+import { SNS } from "aws-sdk";
+import lambdaLog from "lambda-log";
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+export const createPerson = (firstName: string, lastName: string): Person => ({
+  firstName,
+  lastName,
+});
+
 export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+  lambdaLog.options.meta.event = event;
+  lambdaLog.info("Send person event");
+
+  const parameters = createPerson("Test", "Testsson");
+  const snsMessage: SNS.PublishInput = {
+    Subject: "A request for person",
+    Message: JSON.stringify(parameters),
+    TopicArn: "arn:aws:sns:eu-north-1:286643423608:pact-poc",
   };
+  try {
+    await new SNS({ apiVersion: "2010-03-31" }).publish(snsMessage).promise();
+    lambdaLog.info("Sucessfully sent message", { snsMessage });
+  } catch (error) {
+    lambdaLog.info("Fail to sent message", { error });
+  }
 };
