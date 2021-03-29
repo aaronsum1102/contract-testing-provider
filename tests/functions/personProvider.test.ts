5932ffd848d7052baf282639f30b2c35bbeaaa72
aaronsum1102 <sumsx03@gmail.com>
2021-03-29 12:59:32 +0200

    Add pact provider test

---
+++ tests/functions/personProvider.test.ts
@@ -0,0 +1,34 @@
+import { createPerson } from "../../src/functions/personProvider";
+import {
+  MessageProviderPact,
+  PactMessageProviderOptions,
+} from "@pact-foundation/pact";
+import childProcess from "child_process";
+
+describe("Provider test", () => {
+  const gitHash = childProcess
+    .execSync("git rev-parse --short HEAD")
+    .toString()
+    .trim();
+  const testTimeout = 20000;
+  const option: PactMessageProviderOptions = {
+    messageProviders: {
+      "A request for person": () =>
+        Promise.resolve(createPerson("test", "asd")),
+    },
+    provider: "person-provider",
+    providerVersion: gitHash,
+    pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
+    pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+    logLevel: "info",
+  };
+
+  it(
+    "Sent a person",
+    async () => {
+      const messageProvider = new MessageProviderPact(option);
+      await expect(messageProvider.verify()).resolves.not.toThrow();
+    },
+    testTimeout
+  );
+});
